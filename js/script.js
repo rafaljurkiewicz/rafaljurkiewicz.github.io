b36b07284f807cd5e48835e9571d429d45baca06
rafaljurkiewicz <rafal.jurkiewicz87@gmail.com>
2017-11-27 21:07:41 +0100

    cors

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
+var prefix = "https://cors-anywhere.herokuapp.com/";
 var tweetLink = "https://twitter.com/intent/tweet?text=";
 var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 
 function getQuote() {
-    $.getJSON(quoteUrl, createTweet);
+    $.getJSON(prefix + quoteUrl, createTweet);
+    $.ajaxSetup({ cache: false });
 }
 
 function createTweet(input) {
